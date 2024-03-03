c3fb6a692d55f29a033731dd1ed3b4fa1b8e5399
William Wingate <wtwingate@gmail.com>
2024-03-03 01:53:43 -0500

    Write functions to count words and letters in text

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("hello, world")
+import re
+
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+
+def count_letters(text):
+    letters = {}
+
+    for letter in text.lower():
+        if letter in letters:
+            letters[letter] += 1
+        elif re.match("[a-z]", letter):
+            letters[letter] = 1
+    return letters
+
+
+def print_report():
+    pass
+
+
+def main():
+    with open("./books/frankenstein.txt") as f:
+        file_contents = f.read()
+        word_count = count_words(file_contents)
+        letter_count = count_letters(file_contents)
+
+
+main()
