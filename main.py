2189d8973b464cec7cc2a5e837a391e0ba8947de
William Wingate <wtwingate@gmail.com>
2024-03-03 21:13:20 -0500

    Write function to sort and print results

--- main.py
+++ main.py
@@ -14,17 +14,37 @@ def count_letters(text):
     return letters
 
 
-def print_report():
-    pass
+def print_report(book, word_count, letter_count):
+    print(f"---Begin report of {book}---")
+    print()
+    print(f"{word_count} words found in this document")
+    print()
+    for letter in letter_count:
+        for key, value in letter.items():
+            print(f"The letter '{key}' was found {value} times")
+    print()
+    print("---End report---")
+
+
+def sort_results(letter_count):
+    sorted_list = []
+    for key, value in letter_count.items():
+        sorted_list.append({key: value})
+    sorted_list.sort(key=sort_on, reverse=True)
+    return sorted_list
+
+
+def sort_on(dict):
+    return list(dict.values())[0]
 
 
 def main():
-    with open("./books/frankenstein.txt") as f:
+    book = "books/frankenstein.txt"
+    with open(book) as f:
         file_contents = f.read()
         word_count = count_words(file_contents)
-        print(word_count)
-        letter_count = count_letters(file_contents)
-        print(letter_count)
+        letter_count = sort_results(count_letters(file_contents))
+        print_report(book, word_count, letter_count)
 
 
 main()
